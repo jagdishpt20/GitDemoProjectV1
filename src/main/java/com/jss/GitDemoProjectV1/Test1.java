bc4fe479b5c1b8ccd81de2065e618710d7c14a9c
Jagdish <jagdishpt20@gmail.com>
2020-07-23 10:25:30 +0530

    First commit

---
+++ src/main/java/com/jss/GitDemoProjectV1/Test1.java
@@ -0,0 +1,19 @@
+package com.jss.GitDemoProjectV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test1 
+{
+    public static void main( String[] args )
+    {	
+    	String hungry = "yes";
+    	if(hungry.equals("yes")) {
+    		System.out.println("Please eat something");
+    	}
+    	else {
+    		System.out.println("Do work...");
+    	}
+    }
+}
