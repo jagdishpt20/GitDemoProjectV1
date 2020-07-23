44df5a3f487215b924b54e6e641aa52b6750ae4e
Jagdish <jagdishpt20@gmail.com>
2020-07-23 13:08:41 +0530

    Added Test2 class to BranchX

---
+++ src/main/java/com/jss/GitDemoProjectV1/Test2.java
@@ -0,0 +1,19 @@
+package com.jss.GitDemoProjectV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test2 
+{
+    public static void main( String[] args )
+    {	
+    	String hungry = "yes";
+    	if(hungry.equals("yes")) {
+    		System.out.println("Please eat something");
+    	}
+    	else {
+    		System.out.println("Do work...");
+    	}
+    }
+}
