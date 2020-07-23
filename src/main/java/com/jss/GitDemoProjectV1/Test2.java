16fcd1183f4c1695ac1eaaad74520b6f22fb3fde
Jagdish <jagdishpt20@gmail.com>
2020-07-23 13:51:46 +0530

    Added additional checks...

--- src/main/java/com/jss/GitDemoProjectV1/Test2.java
+++ src/main/java/com/jss/GitDemoProjectV1/Test2.java
@@ -9,7 +9,7 @@ public class Test2
     public static void main( String[] args )
     {	
     	String hungry = "yes";
-    	if(hungry.equals("yes")) {
+    	if(hungry.equals("yes") || hungry.equals("Y") || hungry.equals("y")) {
     		System.out.println("Please eat something");
     	}
     	else {
